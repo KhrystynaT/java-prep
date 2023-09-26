75e95efb16aedbaacef847f026e7aa3c89df9b24
KhrystynaT <tyvoniuk.khrystyna@gmail.com>
2023-09-26 16:50:27 +0200

    + escapes and comments

--- practice/Main.java
+++ practice/Main.java
@@ -1,6 +1,19 @@
 package practice;
 public class Main {
     public static void main(String[] args){
+        
+        System.out.println("cokkie-dookie\n");
+        System.out.println("\tI am the second statement");
+        System.out.println("\"I am the quotes statement\"");
+//checking out the escapes;
+
+
+/*
+ * 
+ * 
+ * 
+ * multi-lined comment
+ */
 
     }
 }
