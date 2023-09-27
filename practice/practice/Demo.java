f7e6d20bbf7e73a382354dc0e2c648e0bbdaeb9b
KhrystynaT <tyvoniuk.khrystyna@gmail.com>
2023-09-27 17:11:08 +0200

    +strings example

---
+++ practice/practice/Demo.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Demo {
+    public static void main(String[] args) {
+        String firstname = "Katte";
+        String lastname = "Tvatte";
+        String fullname = firstname+lastname;
+
+        System.out.print(fullname);
+    }
+}
