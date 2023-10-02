8a850c41c392118558d514dfcfaafd2e01801b00
KhrystynaT <tyvoniuk.khrystyna@gmail.com>
2023-10-02 12:49:43 +0200

    +moving files +pintln

---
+++ practice/practice/Dinosaur.java
@@ -0,0 +1,8 @@
+package practice.practice;
+public class Dinosaur{
+    public static void main (String[] args){
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a dinosaurs");
+    }
+}
